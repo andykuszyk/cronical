d08ca1762fcfd62b22fca6518a2f8e696794a35a
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2021-06-16 21:30:40 +0100

    Generate button now loads calendar from server

--- internal/cronical/html/cronical.js
+++ internal/cronical/html/cronical.js
@@ -1,10 +1,17 @@
-ics_sources = ['http://localhost:8080/filter?ical=aHR0cHM6Ly9mb3JtMy5wYWdlcmR1dHkuY29tL3ByaXZhdGUvZjBiYzBjNGY0N2ViYWY5OWM2MDNiY2NiN2IwYjIyMTUyMzNmZjcwY2FmNzM0ODY3OWZmYTBjMDFlNGZkM2M2YS9mZWVk&exclude=KiA5LTE2ICogKiAq']
+ics = 'http://localhost:8080/filter?ical=aHR0cHM6Ly9mb3JtMy5wYWdlcmR1dHkuY29tL3ByaXZhdGUvZjBiYzBjNGY0N2ViYWY5OWM2MDNiY2NiN2IwYjIyMTUyMzNmZjcwY2FmNzM0ODY3OWZmYTBjMDFlNGZkM2M2YS9mZWVk&exclude=KiA5LTE2ICogKiAq'
 
 function generate() {
     var webcalUrl = $( '#webcal_url' ).val()
     var cronExpression = $( '#cron' ).val()
     console.log('generate clicked with webcal: ' + webcalUrl + ' and cron: ' + cronExpression)
-    $( '#filtered_url' ).val('spam://eggs')
+    webcalUrlBase64 = btoa(webcalUrl)
+    cronExpressionBase64 = btoa(cronExpression)
+    var generatedUrl = location.protocol + '//' + location.host + '/filter?ical=' + webcalUrlBase64 + '&exclude=' + cronExpressionBase64
+    $( '#filtered_url' ).val(generatedUrl)
+
+    data_req(generatedUrl, function(){
+        $('#calendar').fullCalendar('addEventSource', fc_events(this.response))
+    })
 }
 
 function data_req (url, callback) {
@@ -14,15 +21,8 @@ function data_req (url, callback) {
     req.send()
 }
 
-function add_recur_events() {
-    if (sources_to_load_cnt < 1) {
-        $('#calendar').fullCalendar('addEventSource', expand_recur_events)
-    } else {
-        setTimeout(add_recur_events, 30)
-    }
-}
-
 $(document).ready(function() {
+    var d = new Date()
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -30,15 +30,7 @@ $(document).ready(function() {
             right: 'month,agendaWeek,agendaDay'
         },
         defaultView: 'month',
-	defaultDate: '2016-03-01'
+        defaultDate: d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()
     })
-    sources_to_load_cnt = ics_sources.length
-    for (ics of ics_sources) {
-        data_req(ics, function(){
-            $('#calendar').fullCalendar('addEventSource', fc_events(this.response))
-            sources_to_load_cnt -= 1
-        })
-    }
-    add_recur_events()
 })
 
