eae286aa4e667a96d6d764d3633e206e100cc529
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2021-06-16 20:54:54 +0100

    Simple UI for generating UI, lacking functionality still

--- internal/cronical/html/cronical.js
+++ internal/cronical/html/cronical.js
@@ -1,5 +1,11 @@
 ics_sources = ['http://localhost:8080/filter?ical=aHR0cHM6Ly9mb3JtMy5wYWdlcmR1dHkuY29tL3ByaXZhdGUvZjBiYzBjNGY0N2ViYWY5OWM2MDNiY2NiN2IwYjIyMTUyMzNmZjcwY2FmNzM0ODY3OWZmYTBjMDFlNGZkM2M2YS9mZWVk&exclude=KiA5LTE2ICogKiAq']
 
+function generate() {
+    var webcalUrl = $( '#webcal_url' ).val()
+    var cronExpression = $( '#cron' ).val()
+    console.log('generate clicked with webcal: ' + webcalUrl + ' and cron: ' + cronExpression)
+    $( '#filtered_url' ).val('spam://eggs')
+}
 
 function data_req (url, callback) {
     req = new XMLHttpRequest()
